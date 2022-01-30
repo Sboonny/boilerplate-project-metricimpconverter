cdbf2e4dcd6cff7dba68f08880119d9dfed1b06e
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-01-30 17:00:58 +0200

    converted to es5 and formatted

--- convertHandler.js
+++ convertHandler.js
@@ -11,45 +11,47 @@ const checkDivision = (fraction) => {
   let nums = fraction.split("/");
   return nums.length > 2 ? false : nums;
 };
-function ConvertHandler() {
-  this.getNum = function (input) {
-    let result;
+class ConvertHandler {
+  constructor() {
+    this.getNum = function (input) {
+      let result;
 
-    return result;
-  };
+      return result;
+    };
 
-  this.getUnit = function (input) {
-    let result;
+    this.getUnit = function (input) {
+      let result;
 
-    return result;
-  };
+      return result;
+    };
 
-  this.getReturnUnit = function (initUnit) {
-    let result;
+    this.getReturnUnit = function (initUnit) {
+      let result;
 
-    return result;
-  };
+      return result;
+    };
 
-  this.spellOutUnit = function (unit) {
-    let result;
+    this.spellOutUnit = function (unit) {
+      let result;
 
-    return result;
-  };
+      return result;
+    };
 
-  this.convert = function (initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
+    this.convert = function (initNum, initUnit) {
+      const galToL = 3.78541;
+      const lbsToKg = 0.453592;
+      const miToKm = 1.60934;
+      let result;
 
-    return result;
-  };
+      return result;
+    };
 
-  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
-    let result;
+    this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+      let result;
 
-    return result;
-  };
+      return result;
+    };
+  }
 }
 
-module.exports = ConvertHandler;
+export default ConvertHandler;
