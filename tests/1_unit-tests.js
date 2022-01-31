a9e0e73e162ef7547d61d6883a126f9bccf8117d
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-01-31 19:04:54 +0200

    added the test for input number

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,11 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+   suite('Function convertHandler.getNum(input)', function() {
+     test("Whole Number", function(done) {
+       let input = '32L'
+       assert.equal(convertHandler.getNum(input), 32)
+       done()
+     })
+   })
 });
