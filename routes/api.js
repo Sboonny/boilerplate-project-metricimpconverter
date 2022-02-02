3e472ce137a7dcbb90bf22defcb44e758a28fbf6
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-02-02 18:40:57 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { json } from 'body-parser';
 import { expect } from 'chai';
 import ConvertHandler from '../controllers/convertHandler.js';
 
@@ -7,4 +8,24 @@ export default function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('api/convert')
+   .get(function (req, res) {
+     const input = req.query.input;
+     const initNum = convertHandler.getNum(input);
+     const initUnit = convertHandler.getUnit(input);
+     const returnNum = convertHandler.convert(initNum, initUnit);
+     const returnUnit = convertHandler.getReturnUnit(initUnit);
+     const getString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+     
+     const json = {
+      initNum : initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: getString
+     }
+
+     (!initNum && !initUnit)? json = "invalid number and unit" : (!initUnit)? json = "invalid unit":(!initNum)? json = " invalid number" : null
+     res.json(json)
+   })
 };
